898ea61f6debe54e5544ab11d68092307f37176c
Joe Balistreri <jbalistreri@twitter.com>
2021-03-29 09:24:59 -0700

    add stack name

--- start-service.sh
+++ start-service.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -xe
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/app/release
+
+REGION=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq .region -r`
+export INSTANCE_ID=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
+export STACK_NAME=`aws --region $REGION ec2 describe-tags --filters "Name=resource-id,Values=${INSTANCE_ID}" "Name=key,Values=aws:cloudformation:stack-name" | jq -r ".Tags[0].Value"`
+
 npm run start
